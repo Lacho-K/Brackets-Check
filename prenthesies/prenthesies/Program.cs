79302afeb9003ee7b03bb72b3bd69ea8d27207c3
Lacho123 <44599728+Lacho123@users.noreply.github.com>
2021-08-03 15:25:10 +0300

    Add files via upload

---
+++ prenthesies/prenthesies/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MainClass
+{
+    public static void Main()
+    {
+        char[] parentheses = Console.ReadLine().ToCharArray();
+        Dictionary<char, char> closingBrackets = new Dictionary<char, char>() { { ']', '[' }, { ')', '(' }, { '}', '{' } };
+        Stack<char> stack = new Stack<char>();
+        foreach (char bracket in parentheses)
+        {           
+           if (stack.Count > 0 && closingBrackets.ContainsKey(bracket) && stack.Peek() == closingBrackets[bracket])
+           {
+             stack.Pop();
+           }
+           else
+           {
+             stack.Push(bracket);
+           }
+            
+        }
+        if (stack.Count == 0)
+        {
+            Console.WriteLine("YES");
+        }
+        else
+        {
+            Console.WriteLine("NO");
+        }
+    }
+}
